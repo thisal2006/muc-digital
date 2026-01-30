09e3b7b6e126f51a1ee4a7e26dc19dc969efe9dc
Dunura Witharama <dunurawitharama@gmail.com>
2026-01-30 22:20:05 +0530

    Added real-time Firebase listener for truck GPS movement

--- lib/features.garbage_tracking/garbage_tracking_screen.dart
+++ lib/features.garbage_tracking/garbage_tracking_screen.dart
@@ -1,5 +1,7 @@
+import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:firebase_database/firebase_database.dart';
 
 class GarbageTrackingScreen extends StatefulWidget {
   const GarbageTrackingScreen({super.key});
@@ -9,30 +11,60 @@ class GarbageTrackingScreen extends StatefulWidget {
 }
 
 class _GarbageTrackingScreenState extends State<GarbageTrackingScreen> {
-  bool _mapReady = false;
+  final Completer<GoogleMapController> _controller = Completer();
+
+  final DatabaseReference _truckRef =
+  FirebaseDatabase.instance.ref('trucks/truck_01');
+
+  LatLng _truckLocation = const LatLng(6.9271, 79.8612);
+
+  @override
+  void initState() {
+    super.initState();
+    _listenToTruckLocation();
+  }
+
+  void _listenToTruckLocation() {
+    _truckRef.onValue.listen((event) async {
+      final data = event.snapshot.value as Map?;
+
+      if (data != null) {
+        final lat = data['lat'];
+        final lng = data['lng'];
+
+        if (lat != null && lng != null) {
+          setState(() {
+            _truckLocation = LatLng(lat.toDouble(), lng.toDouble());
+          });
+
+          final controller = await _controller.future;
+          controller.animateCamera(
+            CameraUpdate.newLatLng(_truckLocation),
+          );
+        }
+      }
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text("Garbage Truck Tracking"),
-      ),
-      body: Stack(
-        children: [
-          GoogleMap(
-            initialCameraPosition: const CameraPosition(
-              target: LatLng(6.9271, 79.8612),
-              zoom: 14,
-            ),
-            onMapCreated: (controller) {
-              setState(() {
-                _mapReady = true;
-              });
-            },
+      appBar: AppBar(title: const Text("Garbage Truck Live Tracking")),
+      body: GoogleMap(
+        initialCameraPosition: CameraPosition(
+          target: _truckLocation,
+          zoom: 15,
+        ),
+        markers: {
+          Marker(
+            markerId: const MarkerId("truck"),
+            position: _truckLocation,
+            infoWindow: const InfoWindow(title: "Garbage Truck"),
           ),
-          if (!_mapReady)
-            const Center(child: CircularProgressIndicator()),
-        ],
+        },
+        onMapCreated: (controller) {
+          _controller.complete(controller);
+        },
       ),
     );
   }
