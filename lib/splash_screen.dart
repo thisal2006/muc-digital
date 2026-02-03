6cbed660ba23a08c2272651fbd2b51f7553f0af2
w2120257 <sandupama.20231385@iit.ac.lk>
2026-02-03 21:35:27 +0530

    Updated property booking UI and data model...

--- lib/splash_screen.dart
+++ lib/splash_screen.dart
@@ -1,52 +1,52 @@
-import 'package:flutter/material.dart';
-import 'dart:async';
-import 'intro_screens.dart';
-
-class SplashScreen extends StatefulWidget {
-  @override
-  _SplashScreenState createState() => _SplashScreenState();
-}
-
-class _SplashScreenState extends State<SplashScreen> {
-  @override
-  void initState() {
-    super.initState();
-
-    // Timer
-    Timer(const Duration(seconds: 3), () {
-      Navigator.pushReplacement(
-        context,
-        // jumps to Intro Screens
-        MaterialPageRoute(builder: (context) => IntroScreens()),
-      );
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-
-      backgroundColor: const Color(0xFF6CA651),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Image.asset(
-              'assets/images/logo.png',
-              width: 200,
-            ),
-            const SizedBox(height: 20),
-            const Text(
-              "MUC Digital",
-              style: TextStyle(
-                fontSize: 28,
-                fontWeight: FontWeight.bold,
-                color: Colors.white,
-              ),
-            ),
-          ],
-        ),
-      ),
-    );
-  }
-}
+// import 'package:flutter/material.dart';
+// import 'dart:async';
+// //import 'intro_screens.dart';
+//
+// class SplashScreen extends StatefulWidget {
+//   @override
+//   _SplashScreenState createState() => _SplashScreenState();
+// }
+//
+// class _SplashScreenState extends State<SplashScreen> {
+//   @override
+//   void initState() {
+//     super.initState();
+//
+//     // Timer
+//     Timer(const Duration(seconds: 3), () {
+//       Navigator.pushReplacement(
+//         context,
+//         // jumps to Intro Screens
+//        // MaterialPageRoute(builder: (context) => IntroScreens()),
+//       );
+//     });
+//   }
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//
+//       backgroundColor: const Color(0xFF6CA651),
+//       body: Center(
+//         child: Column(
+//           mainAxisAlignment: MainAxisAlignment.center,
+//           children: [
+//             Image.asset(
+//               'assets/images/logo.png',
+//               width: 200,
+//             ),
+//             const SizedBox(height: 20),
+//             const Text(
+//               "MUC Digital",
+//               style: TextStyle(
+//                 fontSize: 28,
+//                 fontWeight: FontWeight.bold,
+//                 color: Colors.white,
+//               ),
+//             ),
+//           ],
+//         ),
+//       ),
+//     );
+//   }
+// }
