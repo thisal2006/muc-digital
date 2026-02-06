87a7695ad526b3f80075daf08c24ae53984369c2
Dunura Witharama <dunurawitharama@gmail.com>
2026-02-07 00:11:03 +0530

    updated realtime dump repository

--- lib/features/dump_points/data/dump_repository.dart
+++ lib/features/dump_points/data/dump_repository.dart
@@ -2,17 +2,27 @@ import 'package:firebase_database/firebase_database.dart';
 import '../domain/dump_point.dart';
 
 class DumpRepository {
-  final _db = FirebaseDatabase.instance.ref("dump_points");
+
+  final DatabaseReference _dumpRef =
+  FirebaseDatabase.instance.ref('dump_points');
 
   Stream<List<DumpPoint>> watchDumpPoints() {
-    return _db.onValue.map((event) {
-      final data = Map<String, dynamic>.from(event.snapshot.value as Map);
 
-      return data.entries.map((entry) {
+    return _dumpRef.onValue.map((event) {
+
+      final data = event.snapshot.value;
+
+      if (data == null) return [];
+
+      final map = Map<String, dynamic>.from(data as Map);
+
+      return map.entries.map((entry) {
+
         return DumpPoint.fromMap(
           entry.key,
           Map<String, dynamic>.from(entry.value),
         );
+
       }).toList();
     });
   }
