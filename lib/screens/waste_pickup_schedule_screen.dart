931773bf5dc42b80756772dc057b2b12a233eb34
Hansaka-co <duvin.20232479@iit.ac.lk>
2026-02-25 01:38:17 +0530

    Created empty WastePickupScheduleScreen with basic Scaffold

---
+++ lib/screens/waste_pickup_schedule_screen.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class WastePickupScheduleScreen extends StatelessWidget {
+  const WastePickupScheduleScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Waste Pickup Schedule'),
+      ),
+      body: const Center(
+        child: Text('Coming soon: Garbage calendar & truck map'),
+      ),
+    );
+  }
+}
