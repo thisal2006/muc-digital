5ca0ff863ddfaf208fe80364c3fe76f933126f43
Dunura Witharama <dunurawitharama@gmail.com>
2026-03-04 22:31:10 +0530

    Enhance dump name typography for better visual prominence

--- lib/features/dump_points/presentation/dump_points_screen.dart
+++ lib/features/dump_points/presentation/dump_points_screen.dart
@@ -281,11 +281,9 @@ class _DumpPointsScreenState extends State<DumpPointsScreen>
 
               Text(
                 dump.name,
-                style:
-                const TextStyle(
-                  fontSize: 20,
-                  fontWeight:
-                  FontWeight.bold,
+                style: const TextStyle(
+                  fontSize: 22,
+                  fontWeight: FontWeight.w700,
                 ),
               ),
 
