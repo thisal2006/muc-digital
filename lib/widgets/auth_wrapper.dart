23ac857082a3cab1260ab263ceead205888eb9ce
Dulshan9 <praveensankalpadulshan0909@gmail.com>
2026-03-10 14:17:01 +0530

    fix(auth_wrapper): correct import path for `auth_service.dart`
    Updated the import path for `auth_service.dart` to point to the `screens/services` directory.

--- lib/widgets/auth_wrapper.dart
+++ lib/widgets/auth_wrapper.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'dart:async';
-import '../services/auth_service.dart';
+import '../screens/services/auth_service.dart';
 import '../screens/services/auth_service.dart';
 import '../screens/auth/sign_in_screen.dart';
 import '../screens/home_screen.dart';
