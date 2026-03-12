17cfdf6572ef7eb1210dcfd84f1e7e9ad8169a42
Dulshan9 <praveensankalpadulshan0909@gmail.com>
2026-03-12 20:42:49 +0530

    Update sign_in_screen.dart
    Corrected the relative import path for `sign_up_screen.dart`.

--- lib/screens/auth/sign_in_screen.dart
+++ lib/screens/auth/sign_in_screen.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
-import '../sign_up_screen.dart';
+import 'sign_up_screen.dart';
 import 'forgot_password_screen.dart'; // ← Make sure this import exists
 
 class SignInScreen extends StatefulWidget {
