ef8016aaff797d604e081d3083b2e0e3b8399325
Dunura Witharama <dunurawitharama@gmail.com>
2026-03-12 23:23:04 +0530

    fix(dump_points): remove unused variable declaration in GoogleMap widget

--- lib/features/dump_points/presentation/dump_points_screen.dart
+++ lib/features/dump_points/presentation/dump_points_screen.dart
@@ -524,7 +524,6 @@ class _DumpPointsScreenState extends State<DumpPointsScreen>
             markers: dumpMarkers,
             myLocationEnabled: true,
             trafficEnabled: true,
-            MapType _mapType = MapType.normal;
             onMapCreated: (controller) {
               mapController = controller;
             },
