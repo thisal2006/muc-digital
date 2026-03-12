793fb639da517f461125fc2520f11eb03ba533dd
w2120257 <sandupama.20231385@iit.ac.lk>
2026-03-12 13:58:51 +0530

    Settings screen fully done

--- lib/screens/auth/settings_screen.dart
+++ lib/screens/auth/settings_screen.dart
@@ -3,8 +3,6 @@ import 'package:flutter/material.dart';
 class SettingsScreen extends StatelessWidget {
   const SettingsScreen({super.key});
 
-  @override
-
   void _showLogoutDialog(BuildContext context) {
     showDialog(
       context: context,
@@ -27,6 +25,7 @@ class SettingsScreen extends StatelessWidget {
     );
   }
 
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
