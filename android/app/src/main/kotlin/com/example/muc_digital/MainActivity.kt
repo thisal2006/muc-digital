527960dff6ddd9a2c73aba1479df733062c3344b
thisal sandayuru <thisal.20231581@iit.ac.lk>
2026-03-12 19:29:50 +0530

    Update MainActivity.kt
    - Switched import from `FlutterFragmentActivity` to `FlutterActivity`.
    - Updated `MainActivity` class definition.

--- android/app/src/main/kotlin/com/example/muc_digital/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/muc_digital/MainActivity.kt
@@ -1,5 +1,7 @@
 package com.example.muc_digital
 
-import io.flutter.embedding.android.FlutterFragmentActivity
+import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity : FlutterActivity()
+class MainActivity: FlutterActivity() {
+}
+}
