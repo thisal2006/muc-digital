a5355e57b0ecb6223b0c7cff67e33933052cc589
thisal sandayuru <thisal.20231581@iit.ac.lk>
2026-03-14 17:58:33 +0530

    Update crematorium_booking_screen.dart

--- lib/features/crematorium booking/crematorium_booking_screen.dart
+++ lib/features/crematorium booking/crematorium_booking_screen.dart
@@ -1,4 +1,3 @@
-/*
 import 'package:flutter/material.dart';
 import 'package:table_calendar/table_calendar.dart';
 import 'crematorium_booking_data.dart';               // Model for passing data
@@ -232,4 +231,4 @@ class _CrematoriumBookingScreenState extends State<CrematoriumBookingScreen> {
       ),
     );
   }
-}*/
+}
