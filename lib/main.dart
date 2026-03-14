7c9847cd3f972967a3506c55e52b9392282257ec
w2120257 <sandupama.20231385@iit.ac.lk>
2026-03-14 16:25:56 +0530

    PROFILE SCREEN ROLLEDBACK ADN NO ERROS

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,7 @@ import 'screens/user_agreement_screen.dart';
 import 'screens/home_screen.dart';
 import 'screens/emergency_screen.dart';
 import 'screens/announcements_screen.dart';
-//import 'screens/profile_screen.dart';
+import 'screens/profile_screen.dart';
 import 'features/Property/screens/property_booking_screen.dart';
 import 'screens/phone_login_screen.dart';
 import 'screens/otp_verification_screen.dart';
@@ -87,7 +87,7 @@ class MUCdigitalApp extends StatelessWidget {
         '/announcements': (context) => const AnnouncementsScreen(),
         '/garbage_tracker': (context) => const GarbageTrackingScreen(),
         '/property_booking': (context) => const PropertyBookingScreen(),
-       // '/profile': (context) => const ProfileScreen(),
+        '/profile': (context) => const ProfileScreen(),
         '/phone_login': (context) => const PhoneLoginScreen(),
         '/otp_verification': (context) => OTPVerificationScreen(
           verificationId: '',
