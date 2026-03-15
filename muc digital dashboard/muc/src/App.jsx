ac3d22695404bb68192957b4dd9b511cdc2a5053
kasun565 <kasunkanchana56@gmail.com>
2026-03-15 21:02:43 +0530

    Update App.jsx

--- muc digital dashboard/muc/src/App.jsx
+++ muc digital dashboard/muc/src/App.jsx
@@ -1,4 +1,8 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import { AuthProvider } from './context/AuthContext';
+import ProtectedRoute from './components/ProtectedRoute';
+import PublicRoute from './components/PublicRoute';
+import Login from './pages/Login/Login';
 import DashboardLayout from './components/layout/DashboardLayout';
 import Home from './pages/Home/Home';
 import Dashboard from './pages/Dashboard/Dashboard';
@@ -7,27 +11,44 @@ import Vehicles from './pages/Vehicles/Vehicles';
 import VehicleTypes from './pages/VehicleTypes/VehicleTypes';
 import Cemetery from './pages/Cemetery/Cemetery';
 import Property from './pages/Property/Property';
+import Notifications from './pages/Notifications/Notifications';
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route element={<DashboardLayout />}>
-          {/* Home */}
-          <Route path="/" element={<Home />} />
+    <AuthProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/login" element={
+            <PublicRoute>
+              <Login />
+            </PublicRoute>
+          } />
 
-          {/* Vehicle Management */}
-          <Route path="/vehicle" element={<Dashboard />} />
-          <Route path="/vehicle/bookings" element={<Bookings />} />
-          <Route path="/vehicle/list" element={<Vehicles />} />
-          <Route path="/vehicle/types" element={<VehicleTypes />} />
+          <Route element={
+            <ProtectedRoute>
+              <DashboardLayout />
+            </ProtectedRoute>
+          }>
+            {/* Home */}
+            <Route path="/" element={<Home />} />
 
-          {/* Other Modules (blank) */}
-          <Route path="/cemetery" element={<Cemetery />} />
-          <Route path="/property" element={<Property />} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
+            {/* Vehicle Management */}
+            <Route path="/vehicle" element={<Dashboard />} />
+            <Route path="/vehicle/bookings" element={<Bookings />} />
+            <Route path="/vehicle/list" element={<Vehicles />} />
+            <Route path="/vehicle/types" element={<VehicleTypes />} />
+
+            {/* Other Modules (blank) */}
+            <Route path="/cemetery" element={<Cemetery />} />
+            <Route path="/property" element={<Property />} />
+            <Route path="/notifications" element={<Notifications />} />
+          </Route>
+
+          {/* Catch-all route to redirect unknown paths */}
+          <Route path="*" element={<Navigate to="/" replace />} />
+        </Routes>
+      </BrowserRouter>
+    </AuthProvider>
   );
 }
 
