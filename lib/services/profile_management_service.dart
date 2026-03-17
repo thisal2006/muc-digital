f6d7d5061cb450fc45d52a279960a5647c3e2099
w2120257 <sandupama.20231385@iit.ac.lk>
2026-03-17 13:34:10 +0530

    updated teh profile management service

--- lib/services/profile_management_service.dart
+++ lib/services/profile_management_service.dart
@@ -1,1 +1,22 @@
-// TODO Implement this library.
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
+class ProfileManagementService {
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+  final FirebaseFirestore _firestore = FirebaseFirestore.instance;
+
+  // Get current user data
+  Stream<DocumentSnapshot> getUserData() {
+    String uid = _auth.currentUser!.uid;
+    return _firestore.collection('users').doc(uid).snapshots();
+  }
+
+  // Update Profile Name and Phone
+  Future<void> updateProfile(String name, String phone) async {
+    String uid = _auth.currentUser!.uid;
+    await _firestore.collection('users').doc(uid).update({
+      'name': name,
+      'phone': phone,
+    });
+  }
+}
